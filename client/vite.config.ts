f8596b68f68e3a9aace5e3e0500d14d6ac7979cc
Andrew McDonald <AndrewMcDonald.Dev@gmail.com>
2022-12-05 12:43:41 -0500

    Attempt at fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url';
+// import { fileURLToPath, URL } from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
@@ -7,9 +7,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), vueJsx()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
+    // resolve: {
+    //     alias: {
+    //         '@': fileURLToPath(new URL('./src', import.meta.url)),
+    //     },
+    // },
 });
