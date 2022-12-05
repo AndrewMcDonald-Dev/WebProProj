d5fc881fdb27cd7fc2658ea82fdbef23e71b3eeb
Andrew McDonald <AndrewMcDonald.Dev@gmail.com>
2022-12-05 14:29:32 -0500

    Added basic layout to StatsVoiew.vue

---
+++ client/src/views/StatsView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="center">
+        <strong> Hello. </strong>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 75vh;
+    font-size: 100px;
+}
+</style>
